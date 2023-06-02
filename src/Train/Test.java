1a3e523af0f7a17d6a859ccde53a44c201914c27
ADMIN <Hoangthiend10xdctd@gmail.com>
2023-06-02 15:41:21 +0700

    Add First commit

---
+++ src/Train/Test.java
@@ -0,0 +1,39 @@
+package Train;
+
+public class Test {
+    //Biến toàn cục, hay còn gọi là biến instance
+	//Không cần trả giá trị, default value là giá trị của kiểu dữ liệu đó 
+	private int bankinh;
+	private int Chuvi;
+	public static String Ten = "Nguyen Hoang Phuong";
+	public Test() {
+		
+	}
+	public void setBanKinh(int bankinh) {	
+			this.bankinh=bankinh;	
+	}
+	public int getBanKinh() {
+		return bankinh;
+	}
+	public void setChuVi(int Chuvi) {
+		this.Chuvi=Chuvi;
+	}
+	
+	public int TinhChuvi() {
+		return Chuvi+1;
+	}
+	
+	public void In() {
+		//Biến local , được khai báo trong phương thức nào đó, chỉ được sử dụng trong này 
+		int k ;
+		System.out.print(getBanKinh());
+	}
+
+	public static void main(String[] args) {
+		Test ts= new Test();
+		ts.setBanKinh(3);
+		int hehe=ts.TinhChuvi();
+		System.out.print(hehe);
+	}
+
+}
